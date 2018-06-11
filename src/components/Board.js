1070036238091adf26f8a8c75c19918780ddd27b
Wenjie <traxiaojie@gmail.com>
2018-06-11 16:17:19 -0700

    Modified board to display cards from API calls

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,24 +5,56 @@ import axios from 'axios';
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+// import CARD_DATA from '../data/card-data.json';
 
+
+const CARDS_URL = "https://inspiration-board.herokuapp.com/boards/cards/cards";
 class Board extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      cards: [],
-    };
-  }
+		this.state = {
+			cards: [],
+			status: {
+				message: '',
+				type: ''
+			}
+		};
+	}
 
-  render() {
-    return (
-      <div>
-        Board
-      </div>
-    )
-  }
+	componentDidMount() {
+		axios.get(CARDS_URL)
+		.then( (response) => {
+			console.log(response.data);
+			this.setState({ cards: response.data})
+		})
+		.catch( (error) => {
+			console.log(error);
+			this.setState({ message: error.message, type: 'error'})
+		})
+	}
+
+	render() {
+		const cards = this.state.cards.map( (card, index) => {
+			return <Card
+				key={ index }
+				text={ card.card.text }
+				emoji={ card.card.emoji }
+			/>
+		})
+
+		return (
+
+			<div >
+        <section>
+					<NewCardForm />
+				</section>
+				<section className='board'>
+					{ cards }
+				</section>
+			</div>
+		)
+	}
 
 }
 
