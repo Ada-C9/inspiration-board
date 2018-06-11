c3816be2fd4c03f381030b922244dd91372e5ef4
Wenjie <traxiaojie@gmail.com>
2018-06-11 16:18:01 -0700

    Added card component to display a single card

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,13 +5,29 @@ import emoji from 'emoji-dictionary';
 import './Card.css';
 
 class Card extends Component {
-  render() {
-    return (
-      <div className="card">
-        Card
-      </div>
-    )
-  }
+	static propTypes = {
+		text: PropTypes.string,
+		emoji: PropTypes.string,
+		deleteCardCallback: PropTypes.func
+	}
+
+	deleteCard = () => {
+
+	}
+
+	render() {
+		const myEmoji = emoji.getUnicode( this.props.emoji || "smile");
+
+		return (
+			<div className="card">
+				<section className="card__content">
+					<p className="card__content-text">{ this.props.text }</p>
+					<p className="card__content-emoji">{ myEmoji }</p>
+				</section>
+				<button onClick={ this.deleteCard }>Take it!</button>
+			</div>
+		)
+	}
 }
 
 Card.propTypes = {
