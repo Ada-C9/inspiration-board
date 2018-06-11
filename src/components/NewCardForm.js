e1443e52b97a108a6cd96715c2c4fd58a12b22e8
Wenjie <traxiaojie@gmail.com>
2018-06-11 16:18:30 -0700

    Added newcard form and a dropdown emoji menu

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -3,4 +3,51 @@ import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
 import './NewCardForm.css';
 
-const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
+const EMOJI_LIST = ["smile", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog", "cat", "flower", "thinking_face"];
+
+class NewCardForm extends Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			text: '',
+			emoji: ''
+		}
+	}
+
+	onInputChange = (event) => {
+		// console.log(event.target.value);
+		const newState = {};
+		newState[event.target.name] = event.target.value;
+		console.log(newState);
+		this.setState( newState )
+	}
+
+	render() {
+		const emojis = EMOJI_LIST.map( (emo) => {
+			return <option>{emoji.getUnicode(emo)}</option>
+		})
+
+		return(
+			<form className="form">
+				<label htmlFor="text">Messages</label>
+				<input
+					name="text"
+					value={ this.state.emoji }
+					onChange={ this.onInputChange }
+				/>
+				<label htmlFor="emoji">Emoji
+					<select
+						name='emoji'
+						value={ this.state.emoji }
+						onChange={ this.onInputChange }>
+						{ emojis }
+					</select>
+				</label>
+
+			</form>
+		)
+	}
+}
+export default NewCardForm
