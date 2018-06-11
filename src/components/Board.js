e9607498ba4cde1493603cbd4e0daa4bab6fbf7e
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-06-11 16:02:45 -0700

    Use AXIOS to retrieve board data for a particular user.

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,133 +7,34 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
+
 class Board extends Component {
-  constructor() {
+  static propTypes = {
+    boardName: PropTypes.string.boardName
+  };
+
+  constructor(props) {
     super();
 
     this.state = {
-      cards: [
-        {
-          card: {
-            id: 93,
-            text: "Go Get'em!",
-            emoji: "beer"
-          }
-        },
-        {
-          card: {
-            id: 58,
-            text: "asdf",
-            emoji: "beer"
-          }
-        },
-        {
-          card: {
-            id: 57,
-            text: "asdf",
-            emoji: "heart_eyes_cat"
-          }
-        },
-        {
-          card: {
-            id: 41,
-            text: "Skate ipsum dolor sit amet, rail soul skate griptape impossible Steve Olson. Fast plant Andy Howell locals risers full pipe. Betty grind manual hip Dylan Rieder. Darkslide Japan air nose grab baseplate. Bank boardslide varial layback Andrew Reynolds. Tuna-flip disaster Omar Hassan boardslide wax. Vision grind baseplate bigspin rocket air. Poseur acid drop Transworld kickflip no comply. OJ IIs cess slide g-turn slide hang ten.",
-            emoji: "dog"
-          }
-        },
-        {
-          card: {
-            id: 39,
-            text: "Areallyreallyreallylongword",
-            emoji: "heart_eyes_cat"
-          }
-        },
-        {
-          card: {
-            id: 38,
-            text: "Another test",
-            emoji: "heart_eyes"
-          }
-        },
-        {
-          card: {
-            id: 37,
-            text: "Some test text",
-            emoji: ""
-          }
-        },
-        {
-          card: {
-            id: 26,
-            text: "RAAAAD",
-            emoji: "clap"
-          }
-        },
-        {
-          card: {
-            id: 25,
-            text: "GREAT",
-            emoji: "heart_eyes_cat"
-          }
-        },
-        {
-          card: {
-            id: 24,
-            text: "GOOD",
-            emoji: "dog"
-          }
-        },
-        {
-          card: {
-            id: 23,
-            text: "NICE",
-            emoji: "beer"
-          }
-        },
-        {
-          card: {
-            id: 22,
-            text: "",
-            emoji: "heart_eyes"
-          }
-        },
-        {
-          card: {
-            id: 20,
-            text: "",
-            emoji: "heart_eyes_cat"
-          }
-        },
-        {
-          card: {
-            id: 12,
-            text: "you are enough",
-            emoji: null
-          }
-        },
-        {
-          card: {
-            id: 4,
-            text: "Look for the helpers",
-            emoji: null
-          }
-        },
-        {
-          card: {
-            id: 2,
-            text: "BE EXCELLENT TO EACHOTHER",
-            emoji: null
-          }
-        },
-        {
-          card: {
-            id: 1,
-            text: "100",
-            emoji: null
-          }
-        }
-      ],
+      cards: []
     };
+  }
+
+  componentDidMount() {
+    const BOARD_URL = `https://inspiration-board.herokuapp.com/boards/${this.props.boardName}/cards`;
+
+    axios.get(BOARD_URL)
+    .then((response) => {
+      console.log(response);
+      const cards = response.data.slice(0, 100);
+      this.setState({
+        cards: cards
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
 
   render() {
