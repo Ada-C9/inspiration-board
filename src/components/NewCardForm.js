d16ee06c10c87f341001d843639de099f6714446
Dikla <dikla.aharoni@gmail.com>
2018-06-11 17:00:27 -0700

    created the form

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -4,3 +4,68 @@ import emoji from 'emoji-dictionary';
 import './NewCardForm.css';
 
 const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
+
+class NewCardForm extends Component {
+  static propTypes = {
+    addCardCallback: PropTypes.func
+  };
+
+  constructor() {
+    super();
+
+    this.state = {
+      text: '',
+      emoji: '',
+    };
+  }
+
+
+
+  onInputChange = (event) => {
+    let updatedInput = {};
+    updatedInput[event.target.name] = event.target.value
+     this.setState( updatedInput )
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.addCardCallback(this.state);
+    console.log(this.state);
+
+    this.setState({
+      text: '',
+      emoji: '',
+    });
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <div>
+          <label htmlFor="text">Text </label>
+          <input
+          type="text"
+          name="text"
+          value={this.state.text}
+          onChange={this.onInputChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="emoji">Emoji </label>
+          <input
+          type="text"
+          name="emoji"
+          value={this.state.emoji}
+          onChange={this.onInputChange}
+          />
+        </div>
+
+        <div><input type="submit"/> </div>
+      </form>
+    )
+  }
+
+}
+
+export default NewCardForm;
