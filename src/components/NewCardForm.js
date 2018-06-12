5cd06e2b5a124f6b44a0bb4787034196ce11dd24
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-06-12 11:18:17 -0700

    Update state with form input to create new card.

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -6,6 +6,25 @@ import './NewCardForm.css';
 const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
 
 class NewCardForm extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      emoji: ''
+    };
+  }
+
+  onInputChange = (event) => {
+    let newState = {};
+    newState[event.target.name] = event.target.value;
+    this.setState(newState);
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+  }
+
   render() {
     const emoji_options = EMOJI_LIST.map((emoji, index) => {
       return <option key={index} >
@@ -17,11 +36,18 @@ class NewCardForm extends Component {
         <h3>Add a new inspirational note to this board.</h3>
         <div>
           <label htmlFor="text">Message</label>
-          <input type="text" name="text" />
+          <input
+            type="text"
+            name="text"
+            onChange={ this.onInputChange }
+          />
         </div>
         <div>
           <label htmlFor="emoji">Emoji (optional)</label>
-          <select>
+          <select
+            name="emoji"
+            onChange={ this.onInputChange }
+          >
             { emoji_options }
           </select>
         </div>
