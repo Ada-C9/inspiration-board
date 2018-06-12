77e2bbdce03910d2ae2bf575db06d6c993bff6c8
Dikla <dikla.aharoni@gmail.com>
2018-06-12 16:42:05 -0700

    comment the import json

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+// import CARD_DATA from '../data/card-data.json';
+// import Status from './Status';
 
 const Cards_URL = 'https://inspiration-board.herokuapp.com/boards/Dikla/cards'
 class Board extends Component {
@@ -18,18 +19,54 @@ class Board extends Component {
     super();
 
     this.state = {
-      // cards: props.cards
       cards: [],
     };
   }
 
+
+
+  addCard = (cardInfo) => {
+    console.log(cardInfo);
+    axios.post(Cards_URL, cardInfo)
+    .then((response) => {
+      console.log(response.data);
+      this.props.updateStatusCallback(`Successfully added card ${ response.data.card.id}!`, 'success');
+
+      let updatedData = this.state.cards;
+      updatedData.push(response.data);
+      this.setState({cards: updatedData});
+    })
+    .catch((error) => {
+      this.props.updateStatusCallback(`Error adding card ${ cardInfo.id }.`, 'error');
+      this.setState({ error: error.message });
+    });
+  }
+
+  deleteCard = (index, id) => {
+    let updatedCards = this.state.cards;
+    updatedCards.splice(index, 1);
+    this.setState({cards: updatedCards});
+
+    let DELETE_URL = 'https://inspiration-board.herokuapp.com/boards/' + `Dikla/cards/${id}`;
+    axios.delete(DELETE_URL)
+    .then((response) => {
+      console.log(response);
+      this.props.updateStatusCallback('Successfully deleted card...', 'success')
+    })
+    .catch((error) => {
+      this.setState({ error: error.message });
+      this.props.updateStatusCallback(error.message, 'error')
+    });
+  }
+
+
   componentDidMount() {
-    // this.props.updateStatusCallback('Loading cards...', 'success')
+    this.props.updateStatusCallback('Loading cards...', 'success')
     axios.get(Cards_URL)
     .then((response) => {
       console.log("success");
       console.log(response);
-      // this.setState({ cards: response.data });
+
       this.props.updateStatusCallback('Successfully load cards...', 'success')
       const cards = response.data
       this.setState({ cards: cards });
@@ -37,39 +74,41 @@ class Board extends Component {
     .catch((error) => {
       console.log("Error: (')");
       console.log("error");
-      // this.setState({ error: error.message });
+
       this.props.updateStatusCallback(error.message, 'error')
 
     });
   }
 
 
-  addCard = (card) => {
-    let updatedCards = this.state.cards
-    updatedCards.push(card);
-
-    this.setState({cards: updatedCards})
-  }
 
   render() {
     const cards = this.state.cards.map((card, index) => {
-      return <Card key={index}
+      return <Card key={index} index={index}
+      id={card.card.id}
       text={card.card.text}
-      emoji={card.card.emoji} />
+      emoji={card.card.emoji}
+      deleteCallback={this.deleteCard}
+      />
     });
-    return (
-      <div className="board">
-      <NewCardForm addCardCallback={this.addCard}/>
 
-      {cards}
-      </div>
-    )
+
+
+
+    return (
+      <section>
+      <NewCardForm addCardCallback={this.addCard}/>
+        <div className="board">
+        {cards}
+        </div>
+      </section>
+      )
+    }
+
   }
 
-}
+  Board.propTypes = {
 
-Board.propTypes = {
+  };
 
-};
-
-export default Board;
+  export default Board;
