26993675814cb21555abae85cbf8fcf3be6c7fec
Phoebe <phoebe.manning8@gmail.com>
2018-06-12 16:36:55 -0700

    adds test for user input into form

--- src/components/NewCardForm.test.js
+++ src/components/NewCardForm.test.js
@@ -8,4 +8,36 @@ describe('NewCardForm', () => {
     expect(wrapper).toMatchSnapshot();
     wrapper.unmount();
   });
+
+  test('when a user adds text/emoji to form', () => {
+    const wrapper = shallow(<NewCardForm addCardCallback = {()=>{}}/>);
+    const inputFields = [
+      {
+        name: 'text',
+        value: 'You can do this!'
+      },
+      {
+        name: 'emoji',
+        value: 'smile'
+      }
+    ]
+
+    inputFields.forEach( (field) => {
+      let nameField = wrapper.find(`[name="${field.name}"]`);
+
+      nameField.simulate('change', {
+        target: {
+          name: field.name,
+          value: field.value,
+        }
+      });
+
+      wrapper.update()
+      nameField = wrapper.find(`[name="${field.name}"]`);
+
+      expect(nameField.getElement().props.value)
+        .toEqual(field.value);
+
+    });
+  });
 });
