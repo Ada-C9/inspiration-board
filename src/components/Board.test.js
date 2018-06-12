aa0ff82a292f6f48a4639d27797129af8b8efc5a
Brenda Rios <riosbrenda@gmail.com>
2018-06-12 15:54:07 -0700

    added board test to test that the board can be rendered.

---
+++ src/components/Board.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Board from './Board';
+import { shallow } from 'enzyme';
+
+
+describe('Board', () => {
+  test('that it matches an existing snapshot', () => {
+    const wrapper = shallow(<Board boardId={1}  />);
+    expect(wrapper).toMatchSnapshot();
+  })
+})
