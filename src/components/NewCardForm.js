44b074c77736711fe05b49e874fa2ca0c1a42129
Brenda Rios <riosbrenda@gmail.com>
2018-06-12 12:33:34 -0700

    added pencil icon for write a note.

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
+import FaPencil from 'react-icons/lib/fa/pencil'
 import './NewCardForm.css';
 
 const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
@@ -36,6 +37,15 @@ class NewCardForm extends Component {
     });
   }
 
+  generateEmojis = () => {
+    const emojiChoices = EMOJI_LIST.map((emojiText) => {
+      return (
+        <option key={emojiText} value={emojiText}>{emojiText.length > 0 ? emoji.getUnicode(emojiText): ""}</option>
+      );
+    });
+    return emojiChoices;
+  }
+
   render() {
     return (
   <div className="new-card-form">
@@ -46,12 +56,13 @@ class NewCardForm extends Component {
     </div>
     <div >
       <form onFormSubmit={this.onFormSubmit} className="new-card-form__form" >
-        <label htmlFor="text" className="new-card-form__form-label">Note</label>
+        <label htmlFor="text" className="new-card-form__form-label">Write a note <FaPencil/><FaPencil/><FaPencil/></label>
           <textarea name="text" onChange={this.onInputChange} value={this.state.text}
           className="new-card-form__form-textarea" />
         <label htmlFor="emoji" className="new-card-form__form-label">Emoji</label>
           <select name="emoji" onChange={this.onInputChange} value={this.state.emoji}
           className="new-card-form__form-select">
+          {this.generateEmojis()}
 
         </select>
         <button type="submit" className="new-card-form__form-button">Submit Card</button>
