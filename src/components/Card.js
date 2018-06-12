6edf6936e091ca48aec29f1ade6eaaa4ca1b77f7
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-06-12 11:52:04 -0700

    Now displaying emojis

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,15 +8,15 @@ class Card extends Component {
   render() {
     return (
       <div className="card">
-        <h1>{ this.props.text }</h1>
-        <h2>{ this.props.emoji }</h2>
+        <div>{ this.props.text }</div>
+        <div>{ emoji.getUnicode(`${this.props.emoji}`) }</div>
       </div>
     )
   }
 }
 
 Card.propTypes = {
-  text: PropTypes.string.isRequired,
+  text: PropTypes.string,
   emoji: PropTypes.string
 };
 
