89b12dca6140585d19297d2ba9d0fb736b97e4d8
Dikla <dikla.aharoni@gmail.com>
2018-06-11 17:01:35 -0700

    Added status messages

--- src/App.js
+++ src/App.js
@@ -2,46 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 import Board from './components/Board';
 import cardData from './data/card-data.json'
+import Status from './components/Status';
 
 class App extends Component {
-  render() {
-    const cardComponents = < Board cards={cardData} />
-    // const cardsData = [
-    // {
-    //     "board": {
-    //         "id": 8,
-    //         "name": "1"
-    //     }
-    // },
-    // {
-    //     "board": {
-    //         "id": 34,
-    //         "name": "13"
-    //     }
-    // },
-    // {
-    //     "board": {
-    //         "id": 37,
-    //         "name": "2"
-    //     }
-    // },
-    // {
-    //     "board": {
-    //         "id": 46,
-    //         "name": "4"
-    //     }
-    // }];
+  constructor() {
+    super();
 
-    return (
+    // Set state to the static data from props
+    this.state = {
+      cards: [],
+      status: {
+        message: 'loaded the page',
+        type: 'success'
+      }
+    };
+  }
+
+  updateStatus = (message, type) => {
+    this.setState({
+      status: {
+        message: message,
+        type: type
+      }
+    });
+  }
+  render() {
+        return (
       <section>
         <header className="header">
           <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
         </header>
-        {cardComponents}
-        {/*<Board cards={cardsData} />
-          url="https://inspiration-board.herokuapp.com/boards/"
-          boardName={`Ada-Lovelace`}
-          /> */}
+        <Status
+        message={this.state.status.message}
+        type={this.state.status.type}
+      />
+      <Board updateStatusCallback={this.updateStatus}/>
       </section>
     );
   }
