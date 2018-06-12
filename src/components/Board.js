35e06d9e17890fbeb97a89e7aec63ad1c374cd35
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-06-11 21:50:40 -0700

    Using API and using all the css

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,19 +7,71 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
+
+
 class Board extends Component {
+  static propTypes = {
+    url: PropTypes.string.isRequired,
+    boardName: PropTypes.string.isRequired
+  }
+
   constructor() {
     super();
 
     this.state = {
       cards: [],
+      status: {
+        message: "successfuly loaded",
+        type: "success"
+      }
     };
   }
 
+
+
+
+  componentDidMount() {
+
+    let url_boards = this.props.url;
+    let board_name = this.props.boardName;
+
+    axios.get(`${url_boards}/${board_name}/cards`).then((response) =>{
+      console.log("success");
+      console.log(response);
+
+
+
+      const my_data = response.data.map((card) =>{
+        return {text: card.card.text,
+          emoji: card.card.emoji
+        };
+
+
+      });
+
+console.log(my_data);
+
+      this.setState({cards: my_data})
+
+
+    })
+
+
+  }
+
   render() {
+    const cards = this.state.cards.map((card, index) => {
+      return <Card key={index}
+      text = {card.text}
+      emoji = {card.emoji}
+      />
+    }
+    )
+
+
     return (
-      <div>
-        Board
+      <div className="board">
+      {cards}
       </div>
     )
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,17 +5,28 @@ import emoji from 'emoji-dictionary';
 import './Card.css';
 
 class Card extends Component {
+
+static propTypes = {
+  text: PropTypes.string.isRequired,
+  emoji: PropTypes.string
+}
+
   render() {
+    let my_emoji = this.props.emoji
+    if (my_emoji === null) {
+      my_emoji = "";
+    }
+
     return (
       <div className="card">
-        Card
+        <section className="card__content">
+          <p className="card__content-text">{this.props.text}</p>
+          <p className="card__content-emoji">{emoji.getUnicode(my_emoji)}</p>
+        </section>
       </div>
     )
   }
 }
 
-Card.propTypes = {
-
-};
 
 export default Card;
