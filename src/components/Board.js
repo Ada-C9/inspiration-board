15175e635d4fe3aec9d30938ede942840490063d
LetiTran <leticiastran@gmail.com>
2018-06-13 15:33:51 -0700

    Added delete card function

--- src/components/Board.js
+++ src/components/Board.js
@@ -19,7 +19,6 @@ class Board extends Component {
 
   componentDidMount = () => {
     // console.log('Component did mount was called');
-
     // axios.get('').then().catch();
     axios.get('https://inspiration-board.herokuapp.com/boards/leti/cards')
       .then( (response) => {
@@ -36,19 +35,45 @@ class Board extends Component {
   }
 
   renderCardList = () => {
-    console.log(`this.state.cards = ${this.state.cards}`)
+    // console.log(`this.state.cards = ${this.state.cards}`)
       const componentList = this.state.cards.map((card, index) => {
         console.log(card.card.text)
         return (
           <Card
            key={index}
+           index={index}
+           id={card.card.id}
            text={card.card.text}
            emoji={card.card.emoji}
+           deleteCardCallback={this.deleteCard}
           />
         );
       });
       return componentList;
     }
+
+    deleteCard = (id, index) => {
+    let url = 'https://inspiration-board.herokuapp.com/boards/leti/cards/'+id
+    console.log(id);
+    axios.delete(url)
+      .then((response) => {
+        console.log(response);
+        const updatedCards = this.state.cards
+        delete updatedCards[index]
+        this.setState({
+          cards: updatedCards,
+        })
+      })
+      .catch((error) => {
+        this.setState({
+          error: error.message
+        });
+        if (error.message) {
+          // this.renderError();
+          console.log(error.message);
+        }
+      });
+  }
 
   render() {
     return (
@@ -65,4 +90,3 @@ Board.propTypes = {
 };
 
 export default Board;
-  // {this.renderBoardList()}
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,42 +6,33 @@ import './Card.css';
 
 class Card extends Component {
 
-deleteCard = (event) => {
-console.log('button clicked')
-console.log(event)
-// axios.post('https://inspiration-board.herokuapp.com/boards/leti/cards/:card_id', card)
-//   .then((response) => {
-//     // pet successfully added
-//     petList.push(pet);
-//     this.setState({
-//       petList,
-//       message: `Successfully Added ${pet.name}`
-//     });
-//   })
-//   .catch((error) => {
-//     // not successfull
-//     this.setState({
-//       message: error.message,
-//     });
-//
-//   })
-}
+  deleteCard = (event) => {
+    console.log('button clicked')
+    event.preventDefault();
+    console.log(`this.props.id = ${this.props.id}`)
+    this.props.deleteCardCallback(this.props.id, this.props.index)
+  }
+
 
   render() {
     return (
       <section className="card">
-        <article className="card__content">
-          <div className="card__content-text">{this.props.text}</div>
-          <div className="card__content-emoji">{emoji.getUnicode(`${this.props.emoji}`)}</div>
-        </article>
-        <button onClick={this.deleteCard} className="card__delete">X</button>
+      <article className="card__content">
+      <div className="card__content-text">{this.props.text}</div>
+      <div className="card__content-emoji">{emoji.getUnicode(`${this.props.emoji}`)}</div>
+      </article>
+      <button onClick={this.deleteCard} className="card__delete">X</button>
       </section>
     )
   }
 }
 
 Card.propTypes = {
-
+    index: PropTypes.number,
+    id: PropTypes.number,
+    text: PropTypes.string,
+    emoji: PropTypes.string,
+    deleteCardCallback: PropTypes.func,
 };
 
 export default Card;
