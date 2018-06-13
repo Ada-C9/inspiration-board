327e337d75ad31c0340520b4626cd240894ffb7e
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-06-13 14:36:45 -0700

    Display list of boards in dropdown in App component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 import Board from './components/Board';
 import Status from './components/Status';
@@ -7,6 +9,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      boards: [],
+      boardsUrl: 'https://inspiration-board.herokuapp.com/boards/',
       status: {
         message: 'Successfully loaded the page',
         type: 'success'
@@ -23,9 +27,31 @@ class App extends Component {
     });
   }
 
+  componentDidMount() {
+    this.updateStatus('Retrieving inspirational boards', 'success');
+
+    axios.get(this.state.boardsUrl)
+      .then((response) => {
+        this.updateStatus('Successfully retrieved all boards', 'success');
+        console.log(response);
+        let updatedBoards = response.data.map((boardInfo) => {
+          return boardInfo.board;
+        });
+        this.setState({
+          boards: updatedBoards
+        });
+      })
+      .catch((error) => {
+        this.updateStatus(`There was a problem retrieving the boards: ${error.message}`, 'error');
+      });
+  }
+
   render() {
+    const boardOptions = this.state.boards.map((board) => {
+      return <option key={board.id}>{board.name}</option>
+    });
     return (
-      <section>
+      <section className="new-card-form__form-select">
         <header className="header">
           <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
         </header>
@@ -33,8 +59,13 @@ class App extends Component {
           message={ this.state.status.message }
           type={ this.state.status.type }
         />
+        <form>
+          <select>
+            {boardOptions}
+          </select>
+        </form>
         <Board
-          url="https://inspiration-board.herokuapp.com/boards/"
+          url={ this.state.boardsUrl }
           boardName={`Ada-Lovelace`}
           updateStatusCallback={ this.updateStatus }
           />
