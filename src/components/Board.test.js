769b2f0ae44f7f6d7b26c0ed29dfbaf4ae3c1e4a
Dikla <dikla.aharoni@gmail.com>
2018-06-13 15:49:52 -0700

    Added test for Card.js

---
+++ src/components/Board.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Board from './Board';
+import { mount, shallow } from 'enzyme';
+
+
+describe('Board', () => {
+  test('deep mount', () => {
+    const board = mount(
+      <Board updateStatusCallback={()=>{}} />
+    );
+
+    expect(board).toMatchSnapshot();
+
+    board.unmount();
+  });
+
+  test('shallow mount', () => {
+    const board = shallow(
+      <Board updateStatusCallback={()=>{}} />
+    );
+
+    expect(board).toMatchSnapshot();
+
+
+  });
+});
