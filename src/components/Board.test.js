ea9e54e4ea983352abbb93a7f362ab0c29556a20
Samantha Berk <sberk10@gmail.com>
2018-06-13 14:23:08 -0700

    Board.test.js

--- src/components/Board.test.js
+++ src/components/Board.test.js
@@ -4,5 +4,12 @@ import { shallow } from 'enzyme';
 import Board from './Board';
 
 describe('Board', () => {
+  test('it will match the last snapshot', () => {
+    const board = shallow(
+      <Board url="" boardName="" updateStatusCallback={()=>{}}/>
+    );
 
+    expect(board).toMatchSnapshot();
+
+  });
 });
