0848f25035409e84950f34744a92efd926d82009
Cara <cararbrennan@gmail.com>
2018-06-13 16:08:32 -0700

    Added Status messages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import Board from './components/Board';
+import Status from './components/Status';
 
 
 class App extends Component {
+  constructor() {
+  super();
+
+  this.state = {
+    status: {
+      message: 'loaded the page',
+      type: 'success'
+    }
+  }
+}
+
+updateStatus = (message, type) => {
+  this.setState({
+    status: {
+      message: message,
+      type: type
+    }
+  })
+}
   render() {
 
     return (
@@ -11,6 +31,10 @@ class App extends Component {
         <header className="header">
           <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
         </header>
+        <Status
+      message={this.state.status.message}
+      type={this.state.status.type}
+      />
         <Board
           url="https://inspiration-board.herokuapp.com/boards/"
           boardName={`Ada-Lovelace`}
