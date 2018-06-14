e6fae968ccf6e8d06a6044b676b51cdd0942ab72
Dikla <dikla.aharoni@gmail.com>
2018-06-14 16:32:43 -0700

    Added tests

--- src/components/NewCardForm.test.js
+++ src/components/NewCardForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NewCardForm from './NewCardForm';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 describe('NewCardForm', () => {
   test('that it matches an existing snapshot', () => {
@@ -14,4 +14,24 @@ describe('NewCardForm', () => {
     // Remove the component from the DOM (save memory and prevent side effects).
     wrapper.unmount();
   });
+
+  test('Invokes callback on form submission', () => {
+    const callback = jest.fn();
+    const cardForm = shallow(
+      <NewCardForm addCardCallback={callback} />
+    );
+
+    cardForm.find('form').simulate('submit', {
+      preventDefault: () => {}
+    });
+
+    expect(callback).toHaveBeenCalled();
+
+    expect(callback.mock.calls[0][0]).toEqual({
+      text: '',
+      emoji: '',
+    });
+
+    expect(callback.mock.calls[0][1]).toEqual('Just Good Vibes');
+  })
 });
