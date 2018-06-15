089ae3ee6bb821cbaa1a4701dc9b45bbc5a85833
Hannah Cameron <hannahiscoding@gmail.com>
2018-06-15 14:43:47 -0700

    added in newcardform test to check callback function

--- src/components/NewCardForm.test.js
+++ src/components/NewCardForm.test.js
@@ -8,4 +8,26 @@ describe('NewCardForm', () => {
 
     expect(form).toMatchSnapshot();
   })
+
+  test('Invokes callback on form submission', () => {
+    const callback = jest.fn();
+    const cardForm = shallow(
+      <NewCardForm addCardCallback={callback} />
+    );
+
+    cardForm.find('form').simulate('submit', {
+      preventDefault: () => {}
+    });
+
+    expect(callback).toHaveBeenCalled();
+
+
+    // callback.mock.calls is a list of all the times it was invoked
+    // callback.mock.calls[0] is a list of all the arguments passed to the first invocation
+    expect(callback.mock.calls[0][0]).toEqual({
+      text: '',
+      emoji: ''
+    });
+  });
+
 });
