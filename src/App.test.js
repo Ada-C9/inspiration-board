70c34dda95748b20120b7eb4bc7c25a2c088f556
Abiaina <abinnet.a@gmail.com>
2018-06-15 16:44:33 -0700

    more tests are passing now the data leak is stopped...had a deep instead of shallow

--- src/App.test.js
+++ src/App.test.js
@@ -6,9 +6,9 @@ import App from './App';
 describe('App', () => {
 
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const wrapper = shallow(<App />);
+        expect(wrapper).toMatchSnapshot();
+        wrapper.unmount()
   });
 
 });
