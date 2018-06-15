fdc7b962ac1cd862784f48055ed116d6a9c4c58c
Dikla <dikla.aharoni@gmail.com>
2018-06-14 18:30:37 -0700

    Updated tghe card to be stateless function component

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,36 +5,30 @@ import emoji from 'emoji-dictionary';
 import './Card.css';
 
 
-class Card extends Component {
-  static propTypes = {
-    text: PropTypes.string.isRequired,
-    emoji: PropTypes.string,
-    deleteCallback: PropTypes.func,
-    id: PropTypes.string,
-    index: PropTypes.string
-  };
-
-  onCardDelete = () => {
-    this.props.deleteCallback(this.props.index, this.props.id);
+const Card = (props) => {
+  const onCardDelete = () => {
+    props.deleteCallback(props.index, props.id);
   }
-
-  render() {
     return (
       <section className="card">
-      <button className="card__delete" onClick={this.onCardDelete}>x</button>
+      <button className="card__delete" onClick={onCardDelete}>x</button>
       <div className="card__content">
         <h1 className="card__content-text">
-          {this.props.text}</h1>
+          {props.text}</h1>
         <p className="card__content-emoji">
-        {emoji.getUnicode(`${this.props.emoji}`)}</p>
+        {emoji.getUnicode(`${props.emoji}`)}</p>
       </div>
       </section>
     )
   }
-}
+
 
 Card.propTypes = {
-
+  text: PropTypes.string.isRequired,
+  emoji: PropTypes.string,
+  deleteCallback: PropTypes.func,
+  id: PropTypes.string,
+  index: PropTypes.string
 };
 
 export default Card;
