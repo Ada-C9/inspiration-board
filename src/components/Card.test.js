ee17627e41950f5fff9954709d8d1df26a464e72
knockknockhusthere <angela.huxia@gmail.com>
2018-06-15 10:34:50 -0700

    created card snapshot

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,18 +1,18 @@
 
 import React from 'react';
-import NewCardForm from './NewCardForm';
+import Card from './Card';
 import { shallow } from 'enzyme';
 
-describe('NewCardForm', () => {
+describe('Card', () => {
   test('that it matches an existing snapshot', () => {
     // First Mount the Component in the testing DOM
     // Arrange
-    const cardForm = shallow( <NewCardForm addCardCallback={() => {} } />);
+    const card = shallow( <Card addPetCallback={() => {} } />);
 
     // Assert that it looks like the last snapshot
-    expect(cardForm).toMatchSnapshot();
+    expect(card).toMatchSnapshot();
 
     // Remove the component from the DOM (save memory and prevent side effects).
-    cardForm.unmount();
+    card.unmount();
   });
 });
