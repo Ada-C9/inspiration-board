3651b195e98ae958f2de6b1b003b8b12b011b434
Karinna Iniguez <karinnainiguez@gmail.com>
2018-06-15 14:28:00 -0700

    Add Status file and make sure it renders in App.  Not working yet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import Board from './components/Board';
+import Status from './components/Status'
 
 class App extends Component {
-  render() {
-    return (
-      <section>
-        <header className="header">
-          <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
-        </header>
-        <Board
-          url="https://inspiration-board.herokuapp.com/boards/"
-          boardName={`Ada-Lovelace`}
-          />
-      </section>
-    );
-  }
+	render() {
+		return (
+			<section>
+				<header className="header">
+					<h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
+				</header>
+
+				<Status />
+				
+				<Board
+					url="https://inspiration-board.herokuapp.com/boards/"
+					boardName={`Ada-Lovelace`}
+					/>
+			</section>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Status.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Status extends React.Component {
+	render(){
+		return (
+			<h2>THIS IS A STATUS</h2>
+		)
+	}
+}
+
+export default Status;
