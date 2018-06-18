b1617287ef24570dc4e393daeb58ca676bf8789f
0velez0 <mariaivelez@gmail.com>
2018-06-18 16:54:05 -0700

    add CSS selectors to Card and Status files to improve views

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
 
@@ -6,12 +6,18 @@ import './Card.css';
 
 const Card = (props) => {
 
+  const onCardDelete = () => {
+    // TODO
+    // deleteCardCallback()
+  }
+
   return (
-    <section>
-      <div className="card">
-        <p>{props.text}</p>
-        <p>{props.emoji}</p>
+    <section className="card">
+      <div className="card__content">
+        <p className="card__content-text">{props.text}</p>
+        <p className="card__content-emoji">{emoji.getUnicode(`${props.emoji}`)}</p>
       </div>
+        <button className="card__delete" onClick={onCardDelete}></button>
     </section>
   )
 }
