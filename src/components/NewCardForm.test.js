f3acfb6df216cebc446e1d0e575059d762228b69
AngelaPoland <angelalpoland@gmail.com>
2018-06-17 20:16:20 -0700

    adds one interactive test

--- src/components/NewCardForm.test.js
+++ src/components/NewCardForm.test.js
@@ -11,4 +11,34 @@ describe('NewCardForm', () => {
 
     wrapper.unmount();
   });
+
+  test('When a user enters text in the text field the field is updated', () => {
+   // arrange
+   // shallow mounted the wrapper
+
+   const wrapper = shallow(<NewCardForm
+     addCardCallback={()=>{}} />)
+
+     // find the input field
+
+     let nameField = wrapper.find('input[name="text"]');
+
+     // Act
+     nameField.simulate('change', {
+       target: {
+         name: 'text',
+         value: 'Boomer loves you',
+       },
+     });
+
+     //force the onChange event
+     wrapper.update();
+     nameField = wrapper.find('input[name="text"]');
+
+     //assert
+     expect(nameField.getElement().props.value).toEqual('Boomer loves you');
+   });
+
+
+
 });
