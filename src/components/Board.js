775741edbf60e32f82e0c18155474e2ef01f836b
0velez0 <mariaivelez@gmail.com>
2018-06-18 16:54:35 -0700

    add axios post a card functionality

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,14 +5,15 @@ import axios from 'axios';
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+// import CARD_DATA from '../data/card-data.json';
 
 const MY_BOARD_URL = "https://inspiration-board.herokuapp.com/boards/maria/cards"
 
 class Board extends Component {
 
   static propTypes = {
-    boardName: PropTypes.string.isRequired
+    // boardName: PropTypes.string.isRequired,
+    updateStatusCallback: PropTypes.func.isRequired
   };
 
   constructor() {
@@ -25,6 +26,7 @@ class Board extends Component {
   componentDidMount() {
     axios.get(MY_BOARD_URL)
       .then((response) => {
+        console.log('Success!');
         console.log(response);
 
       const newCards = response.data
@@ -36,17 +38,45 @@ class Board extends Component {
       });
   }
 
+  addCard = (card) => {
+    axios.post(MY_BOARD_URL, card)
+      .then((response) => {
+        console.log(response.data);
+        this.props.updateStatusCallback(`Successfully added card`, 'success');
+
+        let updatedCards = this.state.cards;
+        updatedCards.push(response.data);
+        console.log(updatedCards); // TODO 
+
+        this.setState({ cards: updatedCards });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.props.updateStatusCallback(`Error adding card!`, 'error');
+      });
+  }
+
+  deleteCard = (card) => {
+    // TODO
+
+  };
+
   render() {
     const myCards = this.state.cards.map((message, index) => {
       return <Card key={index}
       text={message.card.text}
-      emoji={message.card.emoji} />
+      emoji={message.card.emoji}
+      deleteCardCallback={this.deleteCard} />
     });
 
+    // TODO figure out if { myCards } needs to stay
     return (
-      <div className="board">
-      { myCards }
-      </div>
+      <section>
+        <NewCardForm addCardCallback={this.addCard}/>
+          <div className="board">
+           { myCards }
+          </div>
+      </section>
     )
   }
 }
